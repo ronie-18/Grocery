eb8c9a409393b0ec6f8551877722270dcfc25637
ronie-18 <rounakjana74@gmail.com>
2025-08-25 00:32:44 +0530

    Landmark

--- checkout.js
+++ checkout.js
@@ -698,7 +698,7 @@ const setupEventListeners = () => {
                 // Personal details
                 firstName: document.getElementById('firstName').value.trim(),
                 lastName: document.getElementById('lastName').value.trim(),
-                landmark: document.getElementById('landmark').value.trim(),
+                companyName: document.getElementById('companyName').value.trim(),
 
                 // Address
                 streetAddress: document.getElementById('streetAddress').value.trim(),
